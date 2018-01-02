0875a341b35036dbe9a88da8e507b436fb67e815
kingcos <2821836721v@gmail.com>
2018-01-02 23:36:53 +0800

    Import EasyImagy

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
     name: "SwiftJumper",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // EasyImagy
+        .package(url: "https://github.com/koher/EasyImagy.git", from: "0.4.0-alpha"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftJumper",
-            dependencies: []),
+            dependencies: [
+                "EasyImagy",
+            ]),
     ]
 )
