dcedbfcfcb32166c1bbfd8be7e59d7de106280cb
kingcos <2821836721v@gmail.com>
2018-01-07 12:41:28 +0800

    Add String-Extension for substring with Regex pattern

---
+++ Sources/DeviceControllerKit/Extension/String-Extension.swift
@@ -0,0 +1,26 @@
+//
+//  String-Extension.swift
+//  SwiftJumperPackageDescription
+//
+//  Created by kingcos on 05/01/2018.
+//
+
+import Foundation
+
+extension String {
+    func substringsWithPattern(_ pattern: String) -> [String]? {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern)
+            let results = regex.matches(in: self,
+                                        options: [],
+                                        range: NSRange(location: 0, length: self.count))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+}
